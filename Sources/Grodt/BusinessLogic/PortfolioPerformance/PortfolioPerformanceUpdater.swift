9e4cfdde0a068e99cc1f105473d6eeeb0bb2ec4f
Adam Borbas <adborbas@gmail.com>
2024-11-29 08:17:27 +0100

    Cache prices

--- Sources/Grodt/BusinessLogic/PortfolioPerformance/PortfolioPerformanceUpdater.swift
+++ Sources/Grodt/BusinessLogic/PortfolioPerformance/PortfolioPerformanceUpdater.swift
@@ -63,6 +63,7 @@ class PortfolioPerformanceUpdater: PortfolioHistoricalPerformanceUpdater {
         var datedPerformance = [DatedPortfolioPerformance]()
         guard let earliestTransaction = portfolio.earliestTransaction else { return }
         let dates = dateRangeUntilToday(from: earliestTransaction.purchaseDate)
+        var priceCache = [String: Decimal]()
 
         for date in dates {
             let performanceForDate = try await performanceCalculator.performance(of: portfolio, on: date, priceCache: &priceCache)
