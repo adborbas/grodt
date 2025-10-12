998577fff8f0cdb46c47934a449cab3a058bade0
Adam Borbas <adborbas@gmail.com>
2025-10-12 14:00:56 +0200

    add UserPreference

---
+++ Sources/Grodt/Persistency/Models/UserPreference.swift
@@ -0,0 +1,75 @@
+import Fluent
+import Foundation
+
+final class UserPreference: Model, @unchecked Sendable {
+    static let schema = "user_preferences"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Parent(key: Keys.userID)
+    var user: User
+
+    @Field(key: Keys.data)
+    var data: UserPreferencesPayload
+
+    @Timestamp(key: Keys.createdAt, on: .create)
+    var createdAt: Date?
+
+    @Timestamp(key: Keys.updatedAt, on: .update)
+    var updatedAt: Date?
+
+    init() {}
+
+    init(id: UUID? = nil, userID: User.IDValue, data: UserPreferencesPayload = .init()) {
+        self.id = id
+        self.$user.id = userID
+        self.data = data
+    }
+}
+
+fileprivate extension UserPreference {
+    enum Keys {
+        static let userID: FieldKey = "user_id"
+        static let data: FieldKey = "data"
+        static let createdAt: FieldKey = "created_at"
+        static let updatedAt: FieldKey = "updated_at"
+    }
+}
+
+
+extension UserPreference {
+    struct CreateMigration: AsyncMigration {
+        var name: String { "CreateUserPreference" }
+
+        func prepare(on db: Database) async throws {
+            try await db.schema(UserPreference.schema)
+                .id()
+                .field(UserPreference.Keys.userID, .uuid, .required, .references(User.schema, .id, onDelete: .cascade))
+                .field(UserPreference.Keys.data, .json, .required)
+                .field(UserPreference.Keys.createdAt, .datetime)
+                .field(UserPreference.Keys.updatedAt, .datetime)
+                .unique(on: UserPreference.Keys.userID)
+                .create()
+        }
+
+        func revert(on db: Database) async throws {
+            try await db.schema(UserPreference.schema).delete()
+        }
+    }
+}
+
+struct UserPreferencesPayload: Codable {
+    struct Backup: Codable {
+        struct Transactions: Codable {
+            var enabled: Bool?
+        }
+        var transactions: Transactions?
+    }
+
+    var backup: Backup?
+
+    init(backup: Backup? = nil) {
+        self.backup = backup
+    }
+}
---
+++ Sources/Grodt/Persistency/Models/UserSecret.swift
@@ -0,0 +1,74 @@
+import Fluent
+import Vapor
+
+final class UserSecret: Model, @unchecked Sendable {
+    static let schema = "user_secrets"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Parent(key: Keys.userID)
+    var user: User
+
+    @Field(key: Keys.data)
+    var data: UserSecretsPayload
+
+    @Timestamp(key: Keys.createdAt, on: .create)
+    var createdAt: Date?
+
+    @Timestamp(key: Keys.updatedAt, on: .update)
+    var updatedAt: Date?
+
+    init() {}
+
+    init(id: UUID? = nil, userID: User.IDValue, data: UserSecretsPayload = .init()) {
+        self.id = id
+        self.$user.id = userID
+        self.data = data
+    }
+}
+
+fileprivate extension UserSecret {
+    enum Keys {
+        static let userID: FieldKey = "user_id"
+        static let data: FieldKey = "data"
+        static let createdAt: FieldKey = "created_at"
+        static let updatedAt: FieldKey = "updated_at"
+    }
+}
+
+extension UserSecret {
+    struct CreateMigration: AsyncMigration {
+        var name: String { "CreateUserSecret" }
+
+        func prepare(on db: Database) async throws {
+            try await db.schema(UserSecret.schema)
+                .id()
+                .field(UserSecret.Keys.userID, .uuid, .required, .references(User.schema, .id, onDelete: .cascade))
+                .field(UserSecret.Keys.data, .json, .required)
+                .field(UserSecret.Keys.createdAt, .datetime)
+                .field(UserSecret.Keys.updatedAt, .datetime)
+                .unique(on: UserSecret.Keys.userID)
+                .create()
+        }
+
+        func revert(on db: Database) async throws {
+            try await db.schema(UserSecret.schema).delete()
+        }
+    }
+}
+
+struct UserSecretsPayload: Codable, Sendable {
+    struct Mailjet: Codable, Sendable {
+        var apiKey: String?
+        var apiSecret: String?
+        var fromEmail: String?
+        var fromName: String?
+    }
+
+    var mailjet: Mailjet?
+
+    init(mailjet: Mailjet? = nil) {
+        self.mailjet = mailjet
+    }
+}
